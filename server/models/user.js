ddfd77b7a9fb52c0cd9ecb2ec50f843ebcfe5624
rotimi-babalola <rotimi.babalola@andela.com>
2018-05-18 17:50:37 +0100

    add user table

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+/* eslint no-undef:0 */
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        min: 6,
+      },
+    },
+  }, {});
+  User.associate = (models) => {
+    // associations can be defined here
+    User.hasMany(models.Todo, {
+      foreignKey: 'userId',
+      as: 'userTodos',
+    });
+  };
+  return User;
+};
